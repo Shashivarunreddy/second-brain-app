2fa1172fc23b418c82c6bdbaf3bae2500c335919
Shashivarunreddy <137184492+Shashivarunreddy@users.noreply.github.com>
2024-12-26 15:14:02 +0530

    first commit

---
+++ src/index.ts
@@ -0,0 +1,36 @@
+import express from "express";
+
+const app = express();
+
+app.post("/api/v1/signup", (req, res) => {
+
+})
+
+app.post("/api/v1/signin", (req, res) => {
+
+})
+
+app.post("/api/v1/content", (req, res) => {
+
+})
+
+app.get("/api/v1/content", (req, res) => {
+
+})
+
+app.delete("/api/v1/content", (req, res) => {
+
+})
+
+
+app.post("/api/v1/brain/share", (req, res) => {
+
+})
+
+app.get("/api/v1/brain/:shareLink", (req, res) => {
+
+})
+
+
+
+// mongodb api = mongodb+srv://shashi:shashi@shashi.ou5z6.mongodb.net/?retryWrites=true&w=majority&appName=shashi
